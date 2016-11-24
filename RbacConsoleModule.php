1f62cad646c0c5ca5e4f2f3296fdd1724e12ce42
siripong rungsang <siripong.rungsang@gmail.com>
2016-11-24 11:23:39 +0700

    -first commit

---
+++ RbacConsoleModule.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace rsiripong\rbac;
+
+use yii\base\Module;
+
+/**
+ * Rbac console module.
+ * 
+ * @author Dmitry Erofeev <dmeroff@gmail.com>
+ */
+class RbacConsoleModule extends Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'rsiripong\rbac\commands';
+}
